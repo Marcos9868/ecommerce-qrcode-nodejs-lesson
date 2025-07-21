3e667138bd8ed270b0304a7b1ab43df34d612593
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-07-21 20:37:12 -0300

    Feat: main function
    Add base of initial prompt for user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import prompt from 'prompt';
+import { mainPrompt } from './prompts/prompt-main.js';
 
 async function main() {
+  prompt.get(mainPrompt, async (err, result) => {
+    if (result.select == 1) console.log("QrCode");
+    if (result.select == 2) console.log("Password");
+
+    prompt.start();
+  });
 }
 
 main();
