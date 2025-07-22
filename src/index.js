02240d7a92b45ea83f4e60b4924a3106c2778b5c
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-07-22 19:33:24 -0300

    Fix: option 1
    Change to qr code service generation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import prompt from 'prompt';
 import { mainPrompt } from './prompts/prompt-main.js';
+import { createQrCode } from './services/qr-code/create.js';
 
 async function main() {
   prompt.get(mainPrompt, async (err, result) => {
-    if (result.select == 1) console.log("QrCode");
+    if (result.select == 1) await createQrCode(); 
     if (result.select == 2) console.log("Password");
 
     prompt.start();
